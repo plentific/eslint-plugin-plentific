2e0229571cd4dbc55d0c7cc62e4e86fe755098c4
Caique Sousa <caique.sousa@plentific.com>
2022-11-24 09:49:06 +0000

    Fix cylic example

--- examples/app/modules/a/cyclic-import.ts
+++ examples/app/modules/a/cyclic-import.ts
@@ -1,3 +1,7 @@
 // eslint-disable-next-line plentific/no-cyclic-modules-imports
 import { c } from 'modules/c/cyclic-import' // should be error
+// eslint-disable-next-line plentific/no-cyclic-modules-imports
+import { g } from 'libs/g/cyclic-import' // should be error
+
 export const a: number = c
+export const aa: number = g
