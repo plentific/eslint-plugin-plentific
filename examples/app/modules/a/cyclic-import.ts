9b249745563c96568df3a89083c23d4d6d408bd3
Rostislav Simonik <rostislav.simonik@plentific.com>
2022-11-24 10:49:06 +0000

    chore: just test

--- examples/app/modules/a/cyclic-import.ts
+++ examples/app/modules/a/cyclic-import.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line plentific/no-cyclic-modules-imports
 import { c } from 'modules/c/cyclic-import' // should be error
-// eslint-disable-next-line plentific/no-cyclic-modules-imports
+// just test
 import { g } from 'libs/g/cyclic-import' // should be error
 
 export const a: number = c
